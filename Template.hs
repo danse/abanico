0e84ba4b3d8eff1875e5598b0a2f10a2cda6f3d9
danse <f.occhipinti@gmail.com>
2017-04-17 19:14:18 +0200

    factor out common parts of the markup

--- Template.hs
+++ Template.hs
@@ -3,6 +3,25 @@
 import Text.Blaze.Html5 as H hiding (main)
 import Text.Blaze.Html5.Attributes
 import Text.Blaze.Html.Renderer.Pretty (renderHtml)
+import Control.Monad (mapM)
+
+links = [("https://www.glassdoor.com", "Glassdoor"),
+         ("https://remotelist.io/", "Remote List"),
+         ("https://www.bitwage.com", "Bitwage"),
+         ("http://www.cloudpeeps.com/", "CloudPeeps - not many programmers yet, apparentl"),
+         ("https://www.workingnomads.co/jobs", "Working Nomads"),
+         ("https://www.upwork.com/ab/find-work/", "Upwork"),
+         ("https://angel.co/job-collections/remote", "Angel Remote"),
+         ("https://stackoverflow.com/jobs?ref=jobs-tab", "Stack overflow"),
+         ("https://github.com/engineerapart/TheRemoteFreelancer", "The Remote Freelancer (list of links)"),
+         ("https://twitter.com/search?f=tweets&q=%20%23remote%20%23job&src=typd", "Twitter #remote #job"),
+         ("https://twitter.com/search?f=tweets&q=remote javascript", "Twitter remote javascript"),
+         ("https://jobspresso.co/", "Jobspresso"),
+         ("https://gigster.com/", "Gigster"),
+         ("https://www.askdomino.com/", "Domino"),
+         ("http://facebook.com", "Facebook"),
+         ("http://linkedin.com", "Linkedin"),
+         ("http://hashnode.com", "Hashnode")]
 
 template = renderHtml $ do
   H.head $ do
@@ -11,23 +30,8 @@ template = renderHtml $ do
   body $ do
     p "Main"
     ul $ do
-        li $ a ! href "https://www.glassdoor.com" $ "Glassdoor"
-        li $ a ! href "https://remotelist.io/" $ "Remote List"
-        li $ a ! href "https://www.bitwage.com" $ "Bitwage"
-        li $ a ! href "http://www.cloudpeeps.com/" $ "CloudPeeps - not many programmers yet, apparently"
-        li $ a ! href "https://www.workingnomads.co/jobs" $ "Working Nomads"
-        li $ a ! href "https://www.upwork.com/ab/find-work/" $ "Upwork"
-        li $ a ! href "https://angel.co/job-collections/remote" $ "Angel Remote"
-        li $ a ! href "https://stackoverflow.com/jobs?ref=jobs-tab" $ "Stack overflow"
-        li $ a ! href "https://github.com/engineerapart/TheRemoteFreelancer" $ "The Remote Freelancer (list of links)"
-        li $ a ! href "https://twitter.com/search?f=tweets&q=%20%23remote%20%23job&src=typd" $ "Twitter #remote #job"
-        li $ a ! href "https://twitter.com/search?f=tweets&q=remote javascript" $ "Twitter remote javascript"
-        li $ a ! href "https://jobspresso.co/" $ "Jobspresso"
-        li $ a ! href "https://gigster.com/" $ "Gigster"
-        li $ a ! href "https://www.askdomino.com/" $ "Domino"
-        li $ a ! href "http://facebook.com" $ "Facebook"
-        li $ a ! href "http://linkedin.com" $ "Linkedin"
-        li $ a ! href "http://hashnode.com" $ "Hashnode"
+      mapM (\ (h,t) -> li ( a ! href h $ t)) links
+      pure ()
     p "Low traffic"
     ul $ li $ a ! href "https://www.jobsinnew.tech/langs/haskell/" $ "Jobs in new tech -- Haskell"
 
