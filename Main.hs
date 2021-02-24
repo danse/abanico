6a45a84357a5ae81528490d633513eef33e02abf
danse <f.occhipinti@gmail.com>
2021-02-24 18:35:48 +0100

    convert abanico from a template renderer to a server

--- Main.hs
+++ Main.hs
@@ -2,13 +2,21 @@
 
 import Text.Blaze.Html5 as H hiding (main)
 import Text.Blaze.Html5.Attributes
-import Text.Blaze.Html.Renderer.Pretty (renderHtml)
-import Control.Monad (mapM)
+import Text.Blaze.Html.Renderer.Text (renderHtml)
+import Control.Monad (mapM, void)
+import Web.Scotty (scotty, get, html, file)
+import Paths_abanico (getDataFileName)
+import Web.Browser (openBrowser)
+import Data.Text.Lazy (Text)
 
+import qualified Web.Scotty as Scotty
+
+favorites :: [(Html, AttributeValue)]
 favorites = [
          ("stack overflow", "https://stackoverflow.com/jobs/remote-developer-jobs")
             ]
 
+links :: [(Html, AttributeValue)]
 links = [("hashtagnomads jobs", "https://nomadlist.slack.com/messages/C02SU0WLE"),
          ("d3 contracting", "https://d3js.slack.com/messages/C082ZDBU0/"),
          ("workaline", "https://workaline.com/?search=technical4"),
@@ -17,7 +25,6 @@ links = [("hashtagnomads jobs", "https://nomadlist.slack.com/messages/C02SU0WLE"
          ("remote", "https://remote.com/jobs"),
          ("glassdoor", "https://www.glassdoor.com"),
          ("remote list", "https://remotelist.io/"),
-         ("bitwage", "https://www.bitwage.com"),
          ("cloudpeeps", "http://www.cloudpeeps.com/"),
          ("working nomads", "https://www.workingnomads.co/jobs"),
          ("upwork", "https://www.upwork.com/ab/find-work/"),
@@ -32,13 +39,34 @@ links = [("hashtagnomads jobs", "https://nomadlist.slack.com/messages/C02SU0WLE"
          ("linkedin", "http://linkedin.com"),
          ("hashnode", "http://hashnode.com")]
 
+template :: Text
 template = renderHtml $ do
   H.head $ do
     H.title "Abanico"
     link ! rel "icon" ! type_ "image/png" ! href "favicon.png"
     link ! rel "stylesheet" ! href "style.css"
   body $ do
-    mapM (\ (t,h) -> a ! href h $ t) (favorites ++ links)
-    pure ()
+    void (mapM (\ (t,h) -> a ! href h $ t) (favorites ++ links))
 
-main = putStrLn template
+data Asset   = Style | Favicon
+type PathEnd = String
+type Static  = (PathEnd, FilePath)
+
+app :: [Static] -> Scotty.ScottyM ()
+app statics = do
+  get "/" (Web.Scotty.html template)
+  void (mapM getStatic statics)
+  where getStatic :: Static -> Scotty.ScottyM ()
+        getStatic (e, f) = get (Scotty.literal ('/':e)) (file f)
+
+main :: IO ()
+main = do
+  statics <- mapM (dataStatic . assetPath) assets
+  void (openBrowser "http://localhost:3000")
+  scotty 3000 (app statics)
+  where assets = [Style, Favicon]
+        assetPath :: Asset -> PathEnd
+        assetPath Style   = "style.css"
+        assetPath Favicon = "favicon.png"
+        dataStatic :: PathEnd -> IO Static
+        dataStatic e = getDataFileName e >>= \ f -> pure (e,f)
